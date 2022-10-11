2b21071b286735f2835130b7437e7dad5a047d46
danivent23122020 <dani.vent@orange.fr>
2022-10-11 20:00:00 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -102,18 +102,21 @@ themeButton.addEventListener("click", () => {
 });
 
 // Scroll Reveal Animation
-// const sr = ScrollReveal({
-//     origin: 'top',
-//     distance: '30px',
-//     duration: 2000,
-//     reset: true
-// });
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "30px",
+    duration: 2000,
+    reset: true,
+});
 
-// sr.reveal(`.home__data, .home__img,
-//             .about__data, .about__img,
-//             .services__content, .menu__content,
-//             .app__data, .app__img,
-//             .contact__data, .contact__button,
-//             .footer__content`, {
-//     interval: 200
-// })
+sr.reveal(
+    `.home__data, .home__img,
+            .about__data, .about__img,
+            .services__content, .menu__content,
+            .app__data, .app__img,
+            .contact__data, .contact__button,
+            .footer__content`,
+    {
+        interval: 200,
+    }
+);
